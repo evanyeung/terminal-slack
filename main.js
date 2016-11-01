f8edacbba09c912c48184941990d4812eb326003
Evan Yeung <evan.yeung1@gmail.com>
2016-10-31 23:24:51 -0700

    Change indentation to 2 spaces. Remove uneeded files.

--- main.js
+++ main.js
@@ -1,86 +1,84 @@
 var ui = require('./userInterface.js'),
-    slack = require('./slackClient.js'),
-    fs = require('fs'),
-    components = ui.init(), // ui components
-    users,
-    currentUser,
-    channels,
-    currentChannelId,
+  slack = require('./slackClient.js'),
+  fs = require('fs'),
+  components = ui.init(), // ui components
+  users,
+  currentUser,
+  channels,
+  currentChannelId,
 
-    // generates ids for messages
-    getNextId = (function() {
-        var id = 0;
-        return function() {
-            return id += 1;
-        };
-    })();
+  // generates ids for messages
+  getNextId = (function() {
+    var id = 0;
+    return function() {
+      return id += 1;
+    };
+  })();
 
 slack.init(function(data, ws) {
-    currentUser = data.self;
+  currentUser = data.self;
 
-    // don't update focus until ws is connected
-    // focus on the channel list
-    components.channelList.select(0);
-    components.channelList.focus();
-    // re render screen
+  // don't update focus until ws is connected
+  // focus on the channel list
+  components.channelList.select(0);
+  components.channelList.focus();
+  // re render screen
+  components.screen.render();
+
+  ws.on('message', function(message, flags){
+    message = JSON.parse(message);
+
+    if ('reply_to' in message) {
+      handleSentConfirmation(message);
+    } else if (message.type === 'message') {
+      handleNewMessage(message);
+    }
+  });
+
+  // initialize these event handlers here as they allow functionality
+  // that relies on websockets
+
+  // event handler when message is submitted
+  components.messageInput.on('submit', function(text) {
+    var id = getNextId();
+    components.messageInput.clearValue();
+    components.messageInput.focus();
+    components.chatWindow.insertBottom(
+      '{bold}' + currentUser.name + '{/bold}: ' + text + ' (pending - ' + id +' )'
+    );
+    components.chatWindow.scroll(1);
+
     components.screen.render();
+    ws.send(JSON.stringify({
+      id: id,
+      type: 'message',
+      channel: currentChannelId,
+      text: text
+    }));
+  });
 
-    ws.on('message', function(message, flags){
-        message = JSON.parse(message);
+  // set the user list to the users returned from slack
+  // call here to check against currentUser
+  slack.getUsers(function(error, response, data){
+    if (error || response.statusCode != 200) {
+      console.log('Error: ', error, response || response.statusCode);
+      return;
+    }
 
-        if ('reply_to' in message) {
-            handleSentConfirmation(message);
-        }
-        else if (message.type === 'message') {
-            handleNewMessage(message);
-        }
-    });
+    data = JSON.parse(data);
+    users = [];
+    for(var i = 0; i < data.members.length; i++) {
+      user = data.members[i];
+      if(!user.deleted && user.id != currentUser.id) users.push(user);
+    }
 
-    // initialize these event handlers here as they allow functionality
-    // that relies on websockets
-
-    // event handler when message is submitted
-    components.messageInput.on('submit', function(text) {
-        var id = getNextId();
-        components.messageInput.clearValue();
-        components.messageInput.focus();
-        components.chatWindow.insertBottom(
-            '{bold}' + currentUser.name + '{/bold}: ' + text +
-            ' (pending - ' + id +' )'
-        );
-        components.chatWindow.scroll(1);
-
-        components.screen.render();
-        ws.send(JSON.stringify({
-            id: id,
-            type: 'message',
-            channel: currentChannelId,
-            text: text
-        }));
-    });
-
-    // set the user list to the users returned from slack
-    // call here to check against currentUser
-    slack.getUsers(function(error, response, data){
-        if (error || response.statusCode != 200) {
-            console.log('Error: ', error, response || response.statusCode);
-            return;
-        }
-
-        data = JSON.parse(data);
-        users = [];
-        for(var i = 0; i < data.members.length; i++) {
-            user = data.members[i];
-            if(!user.deleted && user.id != currentUser.id) users.push(user);
-        }
-
-        components.userList.setItems(
-           users.map(function(user) {
-               return user.name;
-           })
-        );
-        components.screen.render();
-    });
+    components.userList.setItems(
+      users.map(function(user) {
+        return user.name;
+      })
+    );
+    components.screen.render();
+  });
 });
 
 // set the channel list
@@ -89,162 +87,162 @@ components.screen.render();
 
 // set the channel list to the channels returned from slack
 slack.getChannels(function(error, response, data){
-    if (error || response.statusCode != 200) {
-        console.log('Error: ', error, response || response.statusCode);
-        return;
-    }
+  if (error || response.statusCode != 200) {
+    console.log('Error: ', error, response || response.statusCode);
+    return;
+  }
 
-    data = JSON.parse(data);
-    channels = [];
-    for(var i = 0; i < data.channels.length; i++) {
-        var channel = data.channels[i];
-        if(!channel.is_archived) channels.push(channel);
-    }
-    components.channelList.setItems(
-        channels.map(function(channel) {
-            return channel.name;
-        })
-    );
-    components.screen.render();
+  data = JSON.parse(data);
+  channels = [];
+  for(var i = 0; i < data.channels.length; i++) {
+    var channel = data.channels[i];
+    if(!channel.is_archived) channels.push(channel);
+  }
+  components.channelList.setItems(
+    channels.map(function(channel) {
+      return channel.name;
+    })
+  );
+  components.screen.render();
 });
 
 // event handler when user selects a channel
 var updateMessages = function(data, markFn) {
-    components.chatWindow.deleteTop(); // remove loading message
+  components.chatWindow.deleteTop(); // remove loading message
 
-    // filter and map the messages before displaying them
-    data.messages
-        .filter(function(item) {
-            return (item.type === 'message');
-        })
-        .map(function(message) {
-            var len = users.length,
-                username;
+  // filter and map the messages before displaying them
+  data.messages
+    .filter(function(item) {
+      return (item.type === 'message');
+    })
+    .map(function(message) {
+      var len = users.length,
+        username;
 
-            // get the author
-            if(message.user === currentUser.id)
-              username = currentUser.name;
-            else
-                for(var i=0; i < len; i++) {
-                    if (message.user === users[i].id) {
-                        username = users[i].name;
-                        break;
-                    }
-                }
+      // get the author
+      if(message.user === currentUser.id)
+        username = currentUser.name;
+      else
+        for(var i=0; i < len; i++) {
+          if (message.user === users[i].id) {
+            username = users[i].name;
+            break;
+          }
+        }
 
-            return {text: message.text, username: username};
-        })
-        .forEach(function(message) {
-            // add messages to window
-            components.chatWindow.unshiftLine(
-                '{bold}' + message.username + '{/bold}: ' + message.text
-            );
-        });
+      return {text: message.text, username: username};
+    })
+    .forEach(function(message) {
+      // add messages to window
+      components.chatWindow.unshiftLine(
+        '{bold}' + message.username + '{/bold}: ' + message.text
+      );
+    });
 
-    // mark the most recently read message
-    if (data.messages.length) {
-        markFn(currentChannelId, data.messages[0].ts);
-    }
+  // mark the most recently read message
+  if (data.messages.length) {
+    markFn(currentChannelId, data.messages[0].ts);
+  }
 
-    // reset messageInput and give focus
-    components.messageInput.clearValue();
-    components.chatWindow.scrollTo(components.chatWindow.getLines().length);
-    components.messageInput.focus();
-    components.screen.render();
+  // reset messageInput and give focus
+  components.messageInput.clearValue();
+  components.chatWindow.scrollTo(components.chatWindow.getLines().length);
+  components.messageInput.focus();
+  components.screen.render();
 };
 
 components.userList.on('select', function(data) {
-    var userName = data.content;
+  var userName = data.content;
 
-    // a channel was selected
-    components.mainWindowTitle.setContent('{bold}' + userName + '{/bold}');
-    components.chatWindow.setContent('Getting messages...');
-    components.screen.render();
+  // a channel was selected
+  components.mainWindowTitle.setContent('{bold}' + userName + '{/bold}');
+  components.chatWindow.setContent('Getting messages...');
+  components.screen.render();
 
-    // get user's id
-    var userId = '';
-    for(var i = 0; i < users.length; i++) {
-        user = users[i];
-        if(user.name === userName) {
-            userId = user.id;
-            break;
-        }
+  // get user's id
+  var userId = '';
+  for(var i = 0; i < users.length; i++) {
+    user = users[i];
+    if(user.name === userName) {
+      userId = user.id;
+      break;
     }
-    slack.openIm(userId, function(error, response, data){
-        data = JSON.parse(data);
-        currentChannelId = data.channel.id;
+  }
+  slack.openIm(userId, function(error, response, data){
+    data = JSON.parse(data);
+    currentChannelId = data.channel.id;
 
-        // load im history
-        slack.getImHistory(currentChannelId, function(error, response, data) {
-            data = JSON.parse(data);
-            updateMessages(data, slack.markIm);
-        });
+    // load im history
+    slack.getImHistory(currentChannelId, function(error, response, data) {
+      data = JSON.parse(data);
+      updateMessages(data, slack.markIm);
     });
+  });
 });
 
 components.channelList.on('select', function(data) {
-    var channelName = data.content;
+  var channelName = data.content;
 
-    // a channel was selected
-    components.mainWindowTitle.setContent('{bold}' + channelName + '{/bold}');
-    components.chatWindow.setContent('Getting messages...');
-    components.screen.render();
+  // a channel was selected
+  components.mainWindowTitle.setContent('{bold}' + channelName + '{/bold}');
+  components.chatWindow.setContent('Getting messages...');
+  components.screen.render();
 
-    // join the selected channel
-    slack.joinChannel(channelName, function(error, response, data) {
-        data = JSON.parse(data);
-        currentChannelId = data.channel.id;
+  // join the selected channel
+  slack.joinChannel(channelName, function(error, response, data) {
+    data = JSON.parse(data);
+    currentChannelId = data.channel.id;
 
-        // get the previous messages of the channel and display them
-        slack.getChannelHistory(currentChannelId, function(error, response, data) {
-            data = JSON.parse(data);
-            updateMessages(data, slack.markChannel);
-        });
+    // get the previous messages of the channel and display them
+    slack.getChannelHistory(currentChannelId, function(error, response, data) {
+      data = JSON.parse(data);
+      updateMessages(data, slack.markChannel);
     });
+  });
 });
 
 // handles the reply to say that a message was successfully sent
 function handleSentConfirmation(message) {
-    // for some reason getLines gives an object with int keys
-    var lines = components.chatWindow.getLines(),
-        keys = Object.keys(lines),
-        line, i;
-    for(i=keys.length - 1; i >= 0; i--){
-        line = lines[keys[i]].split('(pending - ');
-        if (parseInt(line.pop()[0]) === message.reply_to) {
+  // for some reason getLines gives an object with int keys
+  var lines = components.chatWindow.getLines(),
+    keys = Object.keys(lines),
+    line, i;
+  for(i=keys.length - 1; i >= 0; i--){
+    line = lines[keys[i]].split('(pending - ');
+    if (parseInt(line.pop()[0]) === message.reply_to) {
 
-            components.chatWindow.deleteLine(parseInt(keys[i]));
+      components.chatWindow.deleteLine(parseInt(keys[i]));
 
-            if (message.ok) {
-                components.chatWindow.insertLine(i, line.join(''));
-            }
-            else {
-                components.chatWindow.insertLine(i, line.join('') + ' (FAILED)');
-            }
-            break;
-        }
+      if (message.ok) {
+        components.chatWindow.insertLine(i, line.join(''));
+      }
+      else {
+        components.chatWindow.insertLine(i, line.join('') + ' (FAILED)');
+      }
+      break;
     }
-    components.chatWindow.scroll(1);
-    components.screen.render();
+  }
+  components.chatWindow.scroll(1);
+  components.screen.render();
 }
 
 function handleNewMessage(message) {
-    if(message.channel !== currentChannelId) {
-        return;
-    }
+  if(message.channel !== currentChannelId) {
+    return;
+  }
 
-    var len = users.length,
-        username;
+  var len = users.length,
+    username;
 
-    // get the author
-    for(var i=0; i < len; i++) {
-        if (message.user === users[i].id) {
-            username = users[i].name;
-        }
+  // get the author
+  for(var i=0; i < len; i++) {
+    if (message.user === users[i].id) {
+      username = users[i].name;
     }
-    components.chatWindow.insertBottom(
-        '{bold}' + username + '{/bold}: ' + message.text
-    );
-    components.chatWindow.scroll(1);
-    components.screen.render();
+  }
+  components.chatWindow.insertBottom(
+    '{bold}' + username + '{/bold}: ' + message.text
+  );
+  components.chatWindow.scroll(1);
+  components.screen.render();
 }
--- userInterface.js
+++ userInterface.js
@@ -2,237 +2,237 @@ var blessed = require('blessed');
 
 module.exports = {
 
-    init: function() {
-        var screen = blessed.screen({
-                autopadding: true,
-                smartCSR: true,
-                title: 'Slack'
-            }),
+  init: function() {
+    var screen = blessed.screen({
+        autopadding: true,
+        smartCSR: true,
+        title: 'Slack'
+      }),
 
-            container = blessed.box({
-                width: '100%',
-                height: '100%',
-                style: {
-                    fg: '#bbb',
-                    bg: '#1d1f21'
-                }
-            }),
+      container = blessed.box({
+        width: '100%',
+        height: '100%',
+        style: {
+          fg: '#bbb',
+          bg: '#1d1f21'
+        }
+      }),
 
-            sideBar = blessed.box({
-                width: '30%',
-                height: '100%',
-            }),
+      sideBar = blessed.box({
+        width: '30%',
+        height: '100%',
+      }),
 
-            usersBox = blessed.box({
-                width: '100%',
-                height: '40%',
-                top: '60%',
-                border: {
-                    type: 'line'
-                },
-                style: {
-                    border: {
-                        fg: '#888'
-                    }
-                }
-
-            }),
-
-            usersTitle = blessed.text({
-                width: '90%',
-                left: '5%',
-                align: 'center',
-                content: '{bold}Users{/bold}',
-                tags: true
-            }),
-
-            userList = blessed.list({
-                width: '90%',
-                height: '70%',
-                left: '5%',
-                top: '20%',
-                keys: true,
-                vi: true,
-                style: {
-                    selected: {
-                        bg: '#373b41',
-                        fg: '#c5c8c6'
-                    }
-                },
-                tags: true
-            }),
-
-            channelsBox = blessed.box({
-                width: '100%',
-                height: '60%',
-                border: {
-                    type: 'line'
-                },
-                style: {
-                    border: {
-                        fg: '#888'
-                    }
-                }
-
-            }),
-
-            channelsTitle = blessed.text({
-                width: '90%',
-                left: '5%',
-                align: 'center',
-                content: '{bold}Channels{/bold}',
-                tags: true
-            }),
-
-            channelList = blessed.list({
-                width: '90%',
-                height: '85%',
-                left: '5%',
-                top: '10%',
-                keys: true,
-                vi: true,
-                style: {
-                    selected: {
-                        bg: '#373b41',
-                        fg: '#c5c8c6'
-                    }
-                },
-                tags: true
-            }),
-
-            mainWindow = blessed.box({
-                width: '70%',
-                height: '100%',
-                left: '30%',
-                //scrollable: true,
-                border: {
-                    type: 'line'
-                },
-                style: {
-                    border: {
-                        fg: '#888'
-                    }
-                }
-            }),
-
-            mainWindowTitle = blessed.text({
-                width: '90%',
-                tags: true
-            }),
-
-            chatWindow = blessed.box({
-                width: '90%',
-                height: '75%',
-                left: '5%',
-                top: '10%',
-                keys: true,
-                vi: true,
-                scrollable: true,
-                alwaysScroll: true,
-                tags: true
-            }),
-
-            messageInput = blessed.textbox({
-                width: '90%',
-                left: '5%',
-                top: '85%',
-                keys: true,
-                vi: true,
-                inputOnFocus: true,
-                border: {
-                    type: 'line'
-                }
-            });
-
-        channelsBox.append(channelsTitle);
-        channelsBox.append(channelList);
-        usersBox.append(usersTitle);
-        usersBox.append(userList);
-        sideBar.append(channelsBox);
-        sideBar.append(usersBox);
-        mainWindow.append(mainWindowTitle);
-        mainWindow.append(chatWindow);
-        mainWindow.append(messageInput);
-        container.append(sideBar);
-        container.append(mainWindow);
-        screen.append(container);
-
-        function keyBindings(ch, key) {
-            switch (key.full) {
-                case 'escape': process.exit(0);
-                    break;
-                case 'C-u': userList.focus(); // ctrl-u for users
-                    break;
-                case 'C-c': channelList.focus(); // ctrl-c for channels
-                    break;
-                case 'C-w': messageInput.focus(); // ctrl-w for write
-                    break;
-                case 'C-l': chatWindow.focus(); // ctrl-l for message list
-                    break;
-            }
-            return;
+      usersBox = blessed.box({
+        width: '100%',
+        height: '40%',
+        top: '60%',
+        border: {
+          type: 'line'
+        },
+        style: {
+          border: {
+            fg: '#888'
+          }
         }
 
-        // Quit on Escape or Control-C.
-        userList.on('keypress', keyBindings);
-        channelList.on('keypress', keyBindings);
-        chatWindow.on('keypress', keyBindings);
-        messageInput.on('keypress', function(ch, key){
-            if (    key.full === 'escape' ||
-                    key.full === 'C-u'    ||
-                    key.full === 'C-c'    ||
-                    key.full === 'C-w'    ||
-                    key.full === 'C-l'   ) {
-                messageInput.cancel();
-                keyBindings(ch, key);
-            }
-        });
+      }),
 
-        // scrolling in chat window
-        chatWindow.on('keypress', function(ch, key) {
-            if (key.name === 'up') {
-                chatWindow.scroll(-1);
-                screen.render();
-                return;
-            }
-            if (key.name === 'down') {
-                chatWindow.scroll(1);
-                screen.render();
-                return;
-            }
-        });
+      usersTitle = blessed.text({
+        width: '90%',
+        left: '5%',
+        align: 'center',
+        content: '{bold}Users{/bold}',
+        tags: true
+      }),
 
-        // event handlers for focus and blur of inputs
-        var onFocus = function(component) {
-            component.style.border = {'fg': '#cc6666'};
-            screen.render();
-        };
-        var onBlur = function(component) {
-            component.style.border = {'fg': '#888'};
-            screen.render();
-        };
-        userList.on('focus', onFocus.bind(null, usersBox));
-        userList.on('blur', onBlur.bind(null, usersBox));
-        channelList.on('focus', onFocus.bind(null, channelsBox));
-        channelList.on('blur', onBlur.bind(null, channelsBox));
-        messageInput.on('focus', onFocus.bind(null, messageInput));
-        messageInput.on('blur', onBlur.bind(null, messageInput));
-        chatWindow.on('focus', onFocus.bind(null, mainWindow));
-        chatWindow.on('blur', onBlur.bind(null, mainWindow));
+      userList = blessed.list({
+        width: '90%',
+        height: '70%',
+        left: '5%',
+        top: '20%',
+        keys: true,
+        vi: true,
+        style: {
+          selected: {
+            bg: '#373b41',
+            fg: '#c5c8c6'
+          }
+        },
+        tags: true
+      }),
 
-        return {
-            screen: screen,
-            usersBox: usersBox,
-            channelsBox: channelsBox,
-            usersTitle: usersTitle,
-            userList: userList,
-            channelsTitle: channelsTitle,
-            channelList: channelList,
-            mainWindow: mainWindow,
-            mainWindowTitle: mainWindowTitle,
-            chatWindow: chatWindow,
-            messageInput: messageInput
-        };
+      channelsBox = blessed.box({
+        width: '100%',
+        height: '60%',
+        border: {
+          type: 'line'
+        },
+        style: {
+          border: {
+            fg: '#888'
+          }
+        }
+
+      }),
+
+      channelsTitle = blessed.text({
+        width: '90%',
+        left: '5%',
+        align: 'center',
+        content: '{bold}Channels{/bold}',
+        tags: true
+      }),
+
+      channelList = blessed.list({
+        width: '90%',
+        height: '85%',
+        left: '5%',
+        top: '10%',
+        keys: true,
+        vi: true,
+        style: {
+          selected: {
+            bg: '#373b41',
+            fg: '#c5c8c6'
+          }
+        },
+        tags: true
+      }),
+
+      mainWindow = blessed.box({
+        width: '70%',
+        height: '100%',
+        left: '30%',
+        //scrollable: true,
+        border: {
+          type: 'line'
+        },
+        style: {
+          border: {
+            fg: '#888'
+          }
+        }
+      }),
+
+      mainWindowTitle = blessed.text({
+        width: '90%',
+        tags: true
+      }),
+
+      chatWindow = blessed.box({
+        width: '90%',
+        height: '75%',
+        left: '5%',
+        top: '10%',
+        keys: true,
+        vi: true,
+        scrollable: true,
+        alwaysScroll: true,
+        tags: true
+      }),
+
+      messageInput = blessed.textbox({
+        width: '90%',
+        left: '5%',
+        top: '85%',
+        keys: true,
+        vi: true,
+        inputOnFocus: true,
+        border: {
+          type: 'line'
+        }
+      });
+
+    channelsBox.append(channelsTitle);
+    channelsBox.append(channelList);
+    usersBox.append(usersTitle);
+    usersBox.append(userList);
+    sideBar.append(channelsBox);
+    sideBar.append(usersBox);
+    mainWindow.append(mainWindowTitle);
+    mainWindow.append(chatWindow);
+    mainWindow.append(messageInput);
+    container.append(sideBar);
+    container.append(mainWindow);
+    screen.append(container);
+
+    function keyBindings(ch, key) {
+      switch (key.full) {
+        case 'escape': process.exit(0);
+          break;
+        case 'C-u': userList.focus(); // ctrl-u for users
+          break;
+        case 'C-c': channelList.focus(); // ctrl-c for channels
+          break;
+        case 'C-w': messageInput.focus(); // ctrl-w for write
+          break;
+        case 'C-l': chatWindow.focus(); // ctrl-l for message list
+          break;
+      }
+      return;
     }
+
+    // Quit on Escape or Control-C.
+    userList.on('keypress', keyBindings);
+    channelList.on('keypress', keyBindings);
+    chatWindow.on('keypress', keyBindings);
+    messageInput.on('keypress', function(ch, key){
+      if (    key.full === 'escape' ||
+        key.full === 'C-u'    ||
+        key.full === 'C-c'    ||
+        key.full === 'C-w'    ||
+        key.full === 'C-l'   ) {
+          messageInput.cancel();
+          keyBindings(ch, key);
+        }
+    });
+
+    // scrolling in chat window
+    chatWindow.on('keypress', function(ch, key) {
+      if (key.name === 'up') {
+        chatWindow.scroll(-1);
+        screen.render();
+        return;
+      }
+      if (key.name === 'down') {
+        chatWindow.scroll(1);
+        screen.render();
+        return;
+      }
+    });
+
+    // event handlers for focus and blur of inputs
+    var onFocus = function(component) {
+      component.style.border = {'fg': '#cc6666'};
+      screen.render();
+    };
+    var onBlur = function(component) {
+      component.style.border = {'fg': '#888'};
+      screen.render();
+    };
+    userList.on('focus', onFocus.bind(null, usersBox));
+    userList.on('blur', onBlur.bind(null, usersBox));
+    channelList.on('focus', onFocus.bind(null, channelsBox));
+    channelList.on('blur', onBlur.bind(null, channelsBox));
+    messageInput.on('focus', onFocus.bind(null, messageInput));
+    messageInput.on('blur', onBlur.bind(null, messageInput));
+    chatWindow.on('focus', onFocus.bind(null, mainWindow));
+    chatWindow.on('blur', onBlur.bind(null, mainWindow));
+
+    return {
+      screen: screen,
+      usersBox: usersBox,
+      channelsBox: channelsBox,
+      usersTitle: usersTitle,
+      userList: userList,
+      channelsTitle: channelsTitle,
+      channelList: channelList,
+      mainWindow: mainWindow,
+      mainWindowTitle: mainWindowTitle,
+      chatWindow: chatWindow,
+      messageInput: messageInput
+    };
+  }
 
 };
