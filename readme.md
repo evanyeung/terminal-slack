7438ce6fead017a1b22726fc85506ab969c6a8ee
FireInjun <mk@FireInjun.com>
2018-07-25 06:59:25 -0400

    Modified for Simplicity.

--- readme.md
+++ readme.md
@@ -1,67 +1,74 @@
 # Terminal Slack
+
 A terminal interface for Slack.
 
 ![Screenshot of Termianl Slack](screen-shot.png)
 
 ## Controls
-| Command | Key Combination |
-| ------- | --------------- |
-| Move up | `up arrow` or `k` |
-| Down up | `down arrow` or `j` |
-| Search | `/` |
-| Exit | `esc` |
-| Select channels list | `ctrl` + `c` |
-| Select users list | `ctrl` + `u` |
-| Select writing area | `ctrl` + `w` |
-| Select message list | `ctrl` + `l` |
- 
+
+| Command              | Key Combination     |
+| -------------------- | ------------------- |
+| Move up              | `up arrow` or `k`   |
+| Down up              | `down arrow` or `j` |
+| Search               | `/`                 |
+| Exit                 | `esc`               |
+| Select channels list | `ctrl` + `c`        |
+| Select users list    | `ctrl` + `u`        |
+| Select writing area  | `ctrl` + `w`        |
+| Select message list  | `ctrl` + `l`        |
+
 ## Prerequsites
- - [Node](https://nodejs.org/en/) v6.0.0 or higher
- - A [Slack](https://slack.com/) Account
+
+- [Node](https://nodejs.org/en/) v6.0.0 or higher
+- A [Slack](https://slack.com/) Account
 
 ## Setup
-1. Download this repository:
 
-	```
-	git clone https://github.com/evanyeung/terminal-slack.git
-	```
+1.  Download this repository:
 
-2. Enter the directory:
+    ```
+    git clone https://github.com/evanyeung/terminal-slack.git
+    ```
 
-	```
-	cd terminal-slack
-	```
+2.  Enter the directory:
 
-3. Install the package:
+    ```
+    cd terminal-slack
+    ```
 
-	```
-	npm install
-	```
-	
-4. Create your Legacy Slack API token.
+3.  Install the package:
 
-	- Go to the [Slack Legacy Tokens](https://api.slack.com/custom-integrations/legacy-tokens) page
-	- Click **Generate Token**
+    ```
+    npm install
+    ```
 
-5. Install your token on your local machine, inserting your token between the quotes:
+4.  Create your Legacy Slack API token.
 
-	```
-	export SLACK_TOKEN='your-slack-token-here'
-	```
+    - Go to the [Slack Legacy Tokens](https://api.slack.com/custom-integrations/legacy-tokens) page
+    - Click **Generate Token**
 
-6. Run the application: 
+5.  Create a `.env` file (see example at .exenv) and insert the following:
 
-	```
-	node main.js
-	```
-	
-7. Terminal Slack should now launch.
+    ```
+    SLACK_TOKEN='your-slack-token-here'
+    ```
+
+6.  Run the application:
+
+    ```
+    node main.js
+    ```
+
+7.  Terminal Slack should now launch.
 
 ## Troubleshooting
- - **Terminal Slack opens for a second but then closes again**
 
- 	This might be due to your `SLACK_TOKEN` not being recognised. Make sure the put your `SLACK_TOKEN` between the two single quotes when exporting it:
- 	
- 	```
- 	export SLACK_TOKEN='xoxp-254112160503-252950188691-252375361712-6cbf56aada30951a9d310a5f23d032a0'
- 	```
+- **Terminal Slack opens for a second but then closes again**
+
+  ````
+  	This might be due to your `SLACK_TOKEN` not being recognised. Make sure the put your `SLACK_TOKEN` inside the `.env` file you created eariler for this very purpose.
+
+  	```
+  	SLACK_TOKEN='xoxp-254112160503-252950188691-252375361712-6cbf56aada30951a9d310a5f23d032a0'
+  	```
+  ````
--- slackClient.js
+++ slackClient.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const request = require('request');
 const WebSocket = require('ws');
+require('dotenv').config();
 
 const TOKEN = process.env.SLACK_TOKEN;
 
 if (TOKEN === undefined) {
-  console.log( // eslint-disable-line no-console
+  console.log(
+    // eslint-disable-line no-console
     'Error: SLACK_TOKEN undefined. Please add SLACK_TOKEN to the environment variables.'
   );
   process.exit(1);
@@ -15,31 +17,37 @@ if (TOKEN === undefined) {
 function slackRequest(endpoint, query, callback) {
   const qs = query;
   qs.token = TOKEN;
-  request.get({
-    url: `https://slack.com/api/${endpoint}`,
-    qs,
-  }, (error, response, data) => {
-    if (error) {
-      fs.writeFileSync('error_log.txt', error);
-      process.exit(1);
-    }
+  request.get(
+    {
+      url: `https://slack.com/api/${endpoint}`,
+      qs,
+    },
+    (error, response, data) => {
+      if (error) {
+        fs.writeFileSync('error_log.txt', error);
+        process.exit(1);
+      }
 
-    if (response.statusCode !== 200) {
-      fs.writeFileSync('error_log.txt', `Response Error: ${response.statusCode}`);
-      process.exit(1);
-    }
+      if (response.statusCode !== 200) {
+        fs.writeFileSync(
+          'error_log.txt',
+          `Response Error: ${response.statusCode}`
+        );
+        process.exit(1);
+      }
 
-    const parsedData = JSON.parse(data);
-    if (!parsedData.ok) {
-      // can't see console.logs with blessed
-      fs.writeFileSync('error_log.txt', `Error: ${parsedData.error}`);
-      process.exit(1);
-    }
+      const parsedData = JSON.parse(data);
+      if (!parsedData.ok) {
+        // can't see console.logs with blessed
+        fs.writeFileSync('error_log.txt', `Error: ${parsedData.error}`);
+        process.exit(1);
+      }
 
-    if (callback) {
-      callback(error, response, data);
+      if (callback) {
+        callback(error, response, data);
+      }
     }
-  });
+  );
 }
 
 module.exports = {
@@ -58,32 +66,44 @@ module.exports = {
     });
   },
   joinChannel(name, callback) {
-    slackRequest('channels.join', {
-      name,
-    }, (error, response, data) => {
-      if (callback) {
-        callback(error, response, data);
+    slackRequest(
+      'channels.join',
+      {
+        name,
+      },
+      (error, response, data) => {
+        if (callback) {
+          callback(error, response, data);
+        }
       }
-    });
+    );
   },
   getChannelHistory(id, callback) {
-    slackRequest('channels.history', {
-      channel: id,
-    }, (error, response, data) => {
-      if (callback) {
-        callback(error, response, data);
+    slackRequest(
+      'channels.history',
+      {
+        channel: id,
+      },
+      (error, response, data) => {
+        if (callback) {
+          callback(error, response, data);
+        }
       }
-    });
+    );
   },
   markChannel(id, timestamp, callback) {
-    slackRequest('channels.mark', {
-      channel: id,
-      ts: timestamp,
-    }, (error, response, data) => {
-      if (callback) {
-        callback(error, response, data);
+    slackRequest(
+      'channels.mark',
+      {
+        channel: id,
+        ts: timestamp,
+      },
+      (error, response, data) => {
+        if (callback) {
+          callback(error, response, data);
+        }
       }
-    });
+    );
   },
   getUsers(callback) {
     slackRequest('users.list', {}, (error, response, data) => {
@@ -93,31 +113,43 @@ module.exports = {
     });
   },
   openIm(id, callback) {
-    slackRequest('im.open', {
-      user: id,
-    }, (error, response, data) => {
-      if (callback) {
-        callback(error, response, data);
+    slackRequest(
+      'im.open',
+      {
+        user: id,
+      },
+      (error, response, data) => {
+        if (callback) {
+          callback(error, response, data);
+        }
       }
-    });
+    );
   },
   getImHistory(id, callback) {
-    slackRequest('im.history', {
-      channel: id,
-    }, (error, response, data) => {
-      if (callback) {
-        callback(error, response, data);
+    slackRequest(
+      'im.history',
+      {
+        channel: id,
+      },
+      (error, response, data) => {
+        if (callback) {
+          callback(error, response, data);
+        }
       }
-    });
+    );
   },
   markIm(id, timestamp, callback) {
-    slackRequest('im.mark', {
-      channel: id,
-      ts: timestamp,
-    }, (error, response, data) => {
-      if (callback) {
-        callback(error, response, data);
+    slackRequest(
+      'im.mark',
+      {
+        channel: id,
+        ts: timestamp,
+      },
+      (error, response, data) => {
+        if (callback) {
+          callback(error, response, data);
+        }
       }
-    });
+    );
   },
 };
